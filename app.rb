32af84cfc7c5888e694990db3fe320b445144e29
Adrienne Yao <adrienne.yao@icloud.com>
2019-09-24 01:51:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @total = params[:num1] * params[:num2]
+    @total = params[:num1].to_i * params[:num2].to_i
     "#{total}"
   end
 end
